12730289c2b1c44a9c5c3727da0ce21187703794
kashvi25ecstu007-cloud <kashvi25ecstu007@ncuindia.edu>
2025-12-15 19:58:06 +0530

    Create Q17.c

---
+++ Q17.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int a[50], n, p, i;
+
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    scanf("%d", &p);
+
+    for (i = p; i < n - 1; i++)
+        a[i] = a[i + 1];
+    n--;
+
+    for (i = 0; i < n; i++)
+        printf("%d ", a[i]);
+
+    return 0;
+}
